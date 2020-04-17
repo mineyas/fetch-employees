f6983a6ff3f8edb0079b909ef21a17222e595c1a
mineyas <mineyaskha@gmail.com>
2020-04-17 02:19:19 +0200

    realized exercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import DisplayEmployee from './components/DisplayEmployee';
+import sampleEmployee from './components/sampleEmployee';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: sampleEmployee
+    };
+    this.getEmployee = this.getEmployee.bind(this);
+  }
+
+
+  getEmployee() {
+    // Send the request
+    axios.get('https://randomuser.me/api?nat=fr')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          employee: data.results[0],
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DisplayEmployee employee={this.state.employee} />
+        <button type="button" onClick={this.getEmployee}>Get employee</button>
+      </div>
+    );
+  }
 }
 
 export default App;
